48c44f0bf37bf42d317b0256aff20f86a294772e
Sylwia <35842428+Silwiaa@users.noreply.github.com>
2018-11-09 17:46:18 +0100

    update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // SET RANDOME JOKE BY CLICKING BUTTON
 var url = 'http://api.icndb.com/jokes/random',
     button = document.getElementById('get-joke');
+
 button.addEventListener('click', function() {
   getJoke();
 });
@@ -13,11 +14,11 @@ document.addEventListener("DOMContentLoaded", getJoke());
 
 // getJoke FUNCTION
 function getJoke() {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', url);
-  xhr.addEventListener('load', function(){
-    var response = JSON.parse(xhr.response);
-    paragraph.innerHTML = response.value.joke;
-  });
-  xhr.send();
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.addEventListener('load', function(){
+        var response = JSON.parse(xhr.response);
+        paragraph.innerHTML = response.value.joke;
+    });
+xhr.send();
 }
