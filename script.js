66fb68b081b3467f1cc50b008bcd13211dabbf19
Sylwia <35842428+Silwiaa@users.noreply.github.com>
2018-11-11 18:42:32 +0100

    add ajax jQuery

--- script.js
+++ script.js
@@ -1,24 +1,16 @@
-// SET RANDOME JOKE BY CLICKING BUTTON
 var url = 'http://api.icndb.com/jokes/random',
-    button = document.getElementById('get-joke');
+    button = document.getElementById('get-joke'),
+    $button = $('#get-joke').click(function() {
+        getJoke();
+    }),
+    $paragraph = $('#joke');
 
-button.addEventListener('click', function() {
-  getJoke();
-});
-
-// SET JOKE OUTPUT
-var paragraph = document.getElementById('joke');
-
-// SHOW JOKE WHEN WEB IS LOADED
-document.addEventListener("DOMContentLoaded", getJoke());
-
-// getJoke FUNCTION
 function getJoke() {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.addEventListener('load', function(){
-        var response = JSON.parse(xhr.response);
-        paragraph.innerHTML = response.value.joke;
-    });
-xhr.send();
+    $.ajax({
+        method: 'GET',
+        url: url,
+        success: function(res){
+            $paragraph.text(res.value.joke);
+        }
+    });  
 }
